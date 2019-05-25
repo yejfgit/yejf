c50b5e9a0fdb63b9233a95e600914c69c419722a
yejinfu <852700320@qq.com>
2019-05-25 19:52:50 +0800

    1

---
+++ yejf-samples/yejf-samples-docker/src/main/java/com/yejf/YejfSamplesDockerApplication.java
@@ -0,0 +1,13 @@
+package com.yejf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YejfSamplesDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YejfSamplesDockerApplication.class, args);
+	}
+
+}
